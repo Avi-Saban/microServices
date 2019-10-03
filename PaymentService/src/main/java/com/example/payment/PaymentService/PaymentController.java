49736051ee900195861951665f0dbfd5a041c8d9
Avi Saban <Avi.Saban@emc.com>
2019-10-03 08:23:59 +0300

    add instanceId

--- PaymentService/src/main/java/com/example/payment/PaymentService/PaymentController.java
+++ PaymentService/src/main/java/com/example/payment/PaymentService/PaymentController.java
@@ -1,13 +1,17 @@
 package com.example.payment.PaymentService;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PaymentController {
 
+    @Value("${eureka.instance.instanceId}")
+    private String instanceId;
+
     @GetMapping("pay/msg")
     public String payment() {
-        return "Payment-Service";
+        return "Payment-Service instanceId:" + instanceId;
     }
 }
